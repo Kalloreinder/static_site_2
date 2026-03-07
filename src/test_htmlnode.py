85013f0b0504e448dbb0d456e3f2f5089246aef8
Kalloreinder <matthieu.perrelle@gmail.com>
2026-03-07 10:41:02 +0100

    more tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,7 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_print_self(self):
+    def test_print_self_eq(self):
         htmlnode = HTMLNode(
             "<p>",
             "This is an HTML Node",
@@ -17,3 +17,19 @@ class TestHTMLNode(unittest.TestCase):
             {"href": "https://www.google.com"}
         )
         self.assertEqual(htmlnode.props_to_self(), htmlnode2.props_to_self())
+    
+    def test_print_self_not_eq(self):
+        htmlnode = HTMLNode(
+            "<p>",
+            "This is an HTML Node",
+            None,
+            {"href": "https://www.google.com"}
+        )
+        htmlnode2 = HTMLNode(
+            "<p>",
+            "This is not really and HTML Node",
+            None,
+            {"href": "https://www.firefox.com"}
+        )
+        self.assertNotEqual(htmlnode.props_to_self(), htmlnode2.props_to_self())
+    
