53855280232350a491d4afc344e590caedf83d04
Kalloreinder <matthieu.perrelle@gmail.com>
2026-03-09 11:02:59 +0100

    Added parentNode and ran some tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,10 +1,10 @@
 from textnode import TextNode
 class HTMLNode:
     def __init__(self,
-                 tag: str = None,
-                 value: str = None,
-                 children: list[TextNode] = None,
-                 props: dict[str:str] = None):
+                 tag = None,
+                 value = None,
+                 children = None,
+                 props = None):
         self.tag = tag
         self.value = value
         self.children = children
@@ -15,16 +15,13 @@ class HTMLNode:
 
     def props_to_self(self):
         print_props = ""
-        for key, value in self.props.items():
-            print_props += f'{key}="{value}" '
+        if self.props is not None:
+            for key, value in self.props.items():
+                print_props += f'{key}="{value}" '
         return print_props.rstrip()
     
-    def __repr__(self):
-        print(f"Tag: {self.tag}")
-        print(f"Value: {self.value}")
-        for child in self.children:
-            print(f"{child}")
-        print(self.props_to_self())
+    def __repr__(self) -> str:
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
@@ -40,8 +37,23 @@ class LeafNode(HTMLNode):
             return self.value
         return f"<{self.tag}>{self.value}</{self.tag}>"
 
-    def __repr__(self):
-        print(f"Tag: {self.tag}")
-        print(f"Value: {self.value}")
-        print(self.props_to_self())
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        self.tag = tag
+        self.children = children
+        self.props = props
     
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag cannot be None")
+        if self.children is None:
+            raise ValueError("Children cannot be None")
+        html_repr = f"<{self.tag}>"
+        for child in self.children:
+            html_repr += child.to_html()
+        html_repr += f"</{self.tag}>"
+        return html_repr      
